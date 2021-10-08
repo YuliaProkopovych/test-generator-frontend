7ccef8f78e4d0680f4aa53e0a9bd116efafa1253
Yulia <schonebud@gmail.com>
2021-10-08 16:29:02 +0300

    Fetch list of users

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,13 +1,40 @@
-import React from 'react';
-
+import React, { useState, useEffect }  from 'react';
+import config from '../config';
 
 const Home = () => {
 
-  return(
-    <div>
-        <h1>User Lists</h1>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+      fetch(config.apiURL + '\\admins')
+          .then(res => res.json())
+          .then(
+              (data) => {
+                  setIsLoaded(true);
+                  setUsers(data);
+
+              },
+              (error) => {
+                  setIsLoaded(true);
+                  setError(error);
+              }
+          )
+    }, []); if (error) {
+      return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+      return <div>Loading...</div>;
+  } else {
+      return (
+          <ul>
+              {users.map(user => (
+              <li key={user.id}>
+                  {user.email}
+              </li>
+              ))}
+          </ul>
+      );
+  }
 }
 
 export default Home;
--- src/webpages/index.js
+++ src/webpages/index.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Home from './home';
 import User from './user';
+import config from '../config';
 
 
 const Webpages = () => {
